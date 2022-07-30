3fd1baa1dd025c7588f783f2744688e97ea62989
Zara116 <zarema.mindubaeva@yandex.ru>
2022-07-30 20:40:36 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticket = 100;
+       int mile=20;
+       int mileBonus=ticket/mile;
+
+            System.out.println(mileBonus);
+      }
+}
