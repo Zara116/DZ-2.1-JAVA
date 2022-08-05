5e81d3e01a81ecc57a8d09fa9030daa02a8ea16a
Zara116 <zarema.mindubaeva@yandex.ru>
2022-08-05 21:26:52 +0300

    reformated code

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         int ticket = 100;
-       int mile=20;
-       int mileBonus=ticket/mile;
+        int mile = 20;
+        int mileBonus = ticket / mile;
 
-            System.out.println(mileBonus);
-      }
+        System.out.println(mileBonus);
+    }
 }
